e74a0dbcf4ac02bac0c7e9acae134e44c41d623d
Jetross <jetrossneri07@gmail.com>
2026-03-28 09:50:13 +0800

    feat: add font

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,13 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+const myFont = localFont({
+  src: "./fonts/SF-Pro-Display-Regular.otf",
 });
 
 export const metadata: Metadata = {
@@ -22,10 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="en" className={myFont.className}>
+      <body>{children}</body>
     </html>
   );
 }
